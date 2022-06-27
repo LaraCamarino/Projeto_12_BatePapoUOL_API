17702bfe425eb12cfe5f9d2233830df9d74671f3
LaraCamarino <laracamarino@gmail.com>
2022-06-26 23:07:56 -0300

    Sign up

--- index.js
+++ index.js
@@ -16,4 +16,43 @@ mongoClient.connect().then(() => {
 	db = mongoClient.db("banco_UOL");
 });
 
+const participantSchema = joi.object({
+	name: joi.string().required()
+});
+
+app.post("/participants", async (request, response) => {
+	const { name } = request.body;
+
+	const validation = participantSchema.validate(request.body);
+	if (validation.error) {
+		console.log(validation.error.details);
+		response.status(422).send("O nome escolhido não é válido.");
+		return;
+	}
+
+	try {
+		const verifySameName = await db.collection("participants").findOne({ name: name });
+		if (verifySameName) {
+			response.status(409).send(console.log("O nome escolhido já está em utilização."));
+			return;
+		}
+		await db.collection("participants").insertOne({
+			name: name,
+			lastStatus: Date.now()
+		});
+		await db.collection("messages").insertOne({
+			from: name,
+			to: "Todos",
+			text: "entra na sala...",
+			type: "status",
+			time: dayjs().format("HH:mm:ss")
+		});
+		response.status(201).send(console.log("Participante criado com sucesso."));
+		return;
+	}
+	catch (error) {
+		response.status(500).send(console.log(error));
+	}
+});
+
 app.listen(5000, () => console.log("Servidor foi iniciado."));
